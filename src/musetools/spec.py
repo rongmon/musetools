9dcfa235d8578964546d0a294f8b8579a879f839
ashaban96 <arshaban@ncsu.edu>
2019-03-12 01:07:59 -0400

    sqaure - version 1

---
+++ src/musetools/spec.py
@@ -0,0 +1,43 @@
+# importing the required libraries
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.wcs import WCS
+
+fitsfile = input("Enter the path to your file: ")
+a = fits.open(fitsfile)
+
+data = a[1].data  # the spectrum data are included in the data extension of the fits file
+# data.shape   # you can use this command to know the dimensions of your data cube
+hdu_hdr = a[1].header  # reading the header of the data into a variable called hdu_hdr
+# We will transform the pixels' indexes into wavelength array
+crval3 = hdu_hdr['CRVAL3']  # This is the starting value of the wavelength
+crpix3 = hdu_hdr['CRPIX3']  # The pixel that has the value of CRVAL3
+cd3_3 = hdu_hdr['CD3_3']    # The range of wavelength difference delta_lambda
+wavedim = hdu_hdr['NAXIS3'] # The dimension of the data axis 3 (Wavelength)
+# Do it
+wave = crval3 + (crpix3 + np.arange(0, wavedim, 1.0)) * cd3_3  # This array contains the wavelength 
+
+####
+# This is the function which will give us the spectrum of each square
+def sq_spectrum(xcen, ycen, wave, flux_data, squaresize, outfile=None):
+    halfbox = (squaresize - 1)//2
+    flux = flux_data[:,ycen-halfbox:ycen+halfbox+1, xcen-halfbox:xcen+halfbox+1]
+    spec = np.sum(flux, axis=(1,2))
+    fig = plt.figure()
+    plt.title('The spectrum of the square centered at ('+str(xcen)+','+str(ycen)+')')
+    plt.xlabel('Wavelength')
+    plt.ylabel('Normalized Flux')
+    plt.plot(wave[2300:2500],spec[2300:2500])
+    plt.show()
+####
+
+squaresize = int(input("Enter the value of the square side length in pixels: "))
+xcen = int(input("Enter the x-coordinate of your central pixel of the square: "))
+ycen = int(input("Enter the y-coordinate of your central pixel of the square: "))
+
+sq_spectrum(xcen, ycen, wave, data, squaresize)
+
+
+
+
