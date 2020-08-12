a40ac24e08fbf6e36648daf906615a58d162ef90
ashaban96 <arshaban@ncsu.edu>
2020-08-12 12:11:04 -0400

    Updates to util Module. 1- 2D and 3D masking with given contour. 2- White light spectral extraction.

--- src/musetools/util.py
+++ src/musetools/util.py
@@ -11,6 +11,11 @@ import matplotlib.pyplot as plt
 from musetools import io as io
 from astropy.nddata import Cutout2D
 from astropy.wcs import WCS
+rom astropy.modeling import models, fitting
+from linetools.spectra.xspectrum1d import XSpectrum1D
+import warnings
+from matplotlib import path
+import matplotlib.patches as patches
 
 from scipy import interpolate
 
@@ -646,3 +651,148 @@ def trim_cube_with_header(fitsfile,size = (200, 100), position = (200., 250.)):
 
 
     return newflux,newvar,newhdr_flx
+
+
+
+def mask_out(vertices, wl, data_cube):
+    """
+    This function gives out: 2d image of the masked region, and the 3D masked data cube
+    Input:
+    vertices: The vertices (in spatial pixels coordinates) of the contour used for spectral extraction. e.g.: vertices = [(x1,y1), (x2,y2), (x3, y3),.............]
+    wl: 2d white light image of the data cube
+    data_cube: 3D data_cube. It can be either the flux or variance cubes.
+
+    Output:
+    1- Masked 2D white light image
+    2- Masked 3D data cube.
+​
+    """
+​
+    nverts = len(vertices)
+​
+​
+    # Making a 2d mask for the contour around the arc from the white light image
+    sh = wl.shape
+​
+    xx, yy = np.meshgrid(np.arange(sh[1]), np.arange(sh[0]))
+    xx, yy = xx.flatten(), yy.flatten()
+    points = np.vstack(( xx, yy)).T
+​
+    codes = np.ones(nverts, int) * path.Path.LINETO
+    codes[0::(nverts-1)] = path.Path.MOVETO
+    codes[-1] = path.Path.CLOSEPOLY
+    p = path.Path(vertices, codes)
+​
+​
+    grid = p.contains_points(points);
+    mask2d = grid.reshape(sh)
+​
+    # Making a 3D mask for the data cube.
+    q = np.where(mask2d ==True)
+    mask_cube = np.zeros(data_cube.shape)
+    mask_cube[:,q[0],q[1]] = 1.0
+​
+    return wl*mask2d, mask_cube*data_cube
+
+def extract_weighted_spectrum(flux,variance,wave,verbose=False,weights='Gaussian',porder=9):
+    """
+    Args:
+        flux             : 3D kcwi fluxcube in the aperture of extraction
+        variance          : 3D KCWI variance cube in the aperture of extraction
+        wave             : wavelength of the datacube that is fed in
+        verbose          : [default False]: if set True shows the fitted light profile
+        weights          : if = 'Gaussian',fits a Gaussian profile
+                              = 'poly', fits an n order polynomial where n is set by porder
+                              = 'Data', Weights by the raw white light image
+        porder           :  polynomial order [default 9]
+​
+​
+    Returns:
+        xspec            : XSpectrum1D object [optimal extraction]
+    """
+    # Sanity Checks
+    q=np.isnan(variance)
+    variance[q]=0.
+    q=variance<0
+    variance[q]=0.
+​
+    q=np.isnan(flux)
+    flux[q]=0.
+​
+    img=flux.sum(axis=0)
+    img[img<0] =0
+    amp_init = flux.max()
+​
+    image = np.sum(flux, axis = 0)              # We now sum the wavelength within the given interval
+    factor = 10.**(-20.)*(wave[-1] - wave[0]) / (0.2)**2.
+    image_SB = image*factor
+    q_nan = np.isnan(image_SB)
+    image_SB[q_nan] = 0.0
+    image_SB[image_SB<0.0] = 0.0
+​
+​
+    ydim,xdim=image_SB.shape#img.shape
+    stdev_init_x = 0.33 * ydim
+    stdev_init_y = 0.33 * xdim
+    theta=0
+​
+    if (weights=='gaussian' or weights=='Gaussian'):
+        g_init = models.Gaussian2D(amp_init, 5, 5, stdev_init_x, stdev_init_y,theta=10)
+    elif (weights =='poly' or weights=='Poly'):
+        g_init =models.Polynomial2D(degree=porder)#
+    else:
+        g_init = models.Gaussian2D(amp_init, 5, 5, stdev_init_x, stdev_init_y,theta=10)
+​
+​
+    yi, xi = np.indices(image_SB.shape)#img.shape)
+​
+    fit_g = fitting.LevMarLSQFitter()
+    with warnings.catch_warnings():
+        # Ignore model linearity warning from the fitter
+        warnings.simplefilter('ignore')
+        p = fit_g(g_init, xi, yi, image_SB)#img)
+​
+    if verbose == True:
+        # Plot the data with the best-fit model
+        print('Plotting fitted model profile')
+        plt.figure(figsize=(8, 2.5))
+        plt.subplot(1, 3, 1)
+        plt.imshow(img, origin='lower', interpolation='nearest')#, vmin=0, vmax=1.2e3)\
+        plt.title("Data")
+        plt.subplot(1, 3, 2)
+        plt.imshow(p(xi, yi), origin='lower', interpolation='nearest')#, vmin=0, vmax=1.2e3)#, vmin=-1e4,
+           #vmax=5e4)
+        plt.title("Model")
+        plt.subplot(1, 3, 3)
+        plt.imshow(img - p(xi, yi), origin='lower', interpolation='nearest')#, vmin=-10, vmax=10)#, vmin=-1e4,
+         #  vmax=5e4)
+        plt.title("Residual")
+​
+    if (weights =='Data' or weights=='Data'):
+        weights=image_SB#img
+    else:
+        weights = p(xi, yi)
+​
+​
+​
+    w = wave
+    n = len(w)
+    fl = np.zeros(n)
+    sig = np.zeros(n)
+​
+    for wv_ii in range(n):
+        # n_spaxels = np.sum(mask)
+        weights = weights / np.sum(weights)
+        fl[wv_ii] = np.nansum(flux[wv_ii] * weights)  # * n_spaxels
+        sig[wv_ii] = np.sqrt(np.nansum(variance[wv_ii] * (weights ** 2)))  # * n_spaxels
+​
+    # renormalize
+    fl_sum = np.nansum(flux,axis=(1,2))
+    norm = np.sum(fl_sum) / np.sum(fl)
+    fl = fl * norm
+    sig = sig * norm
+​
+     # Object
+    xspec = XSpectrum1D.from_tuple((wave, fl, sig))
+​
+    return xspec
